177e2ed2df636e0736c3df5829edeb4017bc7a11
Dhruv Sharma <dhruv4075@gmail.com>
2024-06-23 17:45:03 +0530

    FIX: The Deliver Product Card not rendering

--- client/src/Pages/Orders.jsx
+++ client/src/Pages/Orders.jsx
@@ -62,7 +62,7 @@ const OrderPage = () => {
                         {orders.map((order) => {
                             return (
                                 <>
-                                    {order.delivered === true &&
+                                    {order.delivered !== true &&
                                         order.supplier == userData.id ?
                                             <OrderCard order={order} /> : null
                                     }
--- client/src/constants/basUrl.js
+++ client/src/constants/basUrl.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:8002/';
+export const BASE_URL = 'http://localhost:8000/';
 // http://144.126.253.88:8000/
